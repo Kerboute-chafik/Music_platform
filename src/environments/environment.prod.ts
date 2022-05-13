8d2bde0022ee38624512a0b030d8621bc5d8cbd5
sosoww1 <kerboutechafik@gmail.com>
2022-05-13 17:54:09 +0100

    'Add_Spotify_Api_Authorize'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,18 @@
 export const environment = {
   production: true
 };
+export const SpotifyConfiguration ={
+  clientId: '080af0dde42146a5bc7f64db806f6d7c',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+    "user-library-read",
+    "playlist-read-private",
+    "playlist-read-collaborative"
+  ]
+}
